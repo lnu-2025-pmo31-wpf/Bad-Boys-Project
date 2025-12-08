be4ce714d2eddebd6d96ecdbe5f7aa6dac9a54c7
Pryadko Andrii <153684501+Hardwik1337@users.noreply.github.com>
2025-12-08 14:41:30 +0200

    Create Log.cs

---
+++ src/App.Data/Models/Log.cs
@@ -0,0 +1,14 @@
+namespace App.Data.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int? MediaItemId { get; set; }
+        public string Action { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        public User User { get; set; }
+        public MediaItem MediaItem { get; set; }
+    }
+}
