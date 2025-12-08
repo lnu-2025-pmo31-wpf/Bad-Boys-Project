6ff3b3e016bc26f8ede4807fe09184051c22b2d0
Pryadko Andrii <153684501+Hardwik1337@users.noreply.github.com>
2025-12-08 14:41:04 +0200

    Create Tag.cs

---
+++ src/App.Data/Models/Tag.cs
@@ -0,0 +1,11 @@
+namespace App.Data.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public int MediaItemId { get; set; }
+        public string Name { get; set; }
+
+        public MediaItem MediaItem { get; set; }
+    }
+}
